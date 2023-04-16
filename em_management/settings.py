9d58881c13dc9dc89c33d877dbc61b0595f5c071
Khngan <phankhanhnganngan@gmail.com>
2023-04-16 11:49:09 +0700

    Implement CRUD department

--- em_management/settings.py
+++ em_management/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'django_em_management',
-        'USER': 'hbstudent',
-        'PASSWORD': 'hbstudent',
+        'USER': 'root',
+        'PASSWORD': 'Pknpknkn270!',
         'HOST': 'localhost',  
         'PORT': '3306',
     }
